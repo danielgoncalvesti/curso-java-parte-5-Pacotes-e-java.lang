cfac37783644f9d15309f1d11494e69c2f9aff59
Daniel Goncalves <dgoncalves@livecare.ca>
2020-04-23 17:55:28 -0300

    chapter 4 is completed - pacote java.lang

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,20 +1,16 @@
 package br.com.bytebank.banco.teste;
 
-import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 
+//br.com.bytebank.banco.teste.TesteContas - Copy Qualified Name
 public class TesteContas {
 
 	public static void main(String[] args) throws SaldoInsuficienteException{
-		//int a = 3;
-		//int b = a / 0;
-		
-		
-		ContaCorrente outra = null;
-		//outra.deposita(200.0);
-		
+
+				
+//		ContaCorrente cct = null;		
 					
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,47 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String vazio = " Alura ";
+		String outroVazio = vazio.trim();
+		System.out.println(vazio.contains("Al"));
+		System.out.println(outroVazio);
+		
+		
+		int a = 3;
+		String nome = "Alura"; //object literal		
+//		String outros = new String("alura");
+
+//		System.out.println(nome.length());
+		
+//		for (int i = 0; i < nome.length(); i++) {
+//			System.out.println(nome.charAt(i));
+//		}
+		
+		
+//		String sub = nome.substring(1);
+//		System.out.println(sub);
+		
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);
+		
+//		char c = nome.charAt(2);
+//		System.out.println(c);
+		
+		
+//		char c = 'a';
+//		char d = 'd';
+//		String outro = nome.replace(c, d);
+		
+//		String outro = nome.toUpperCase();
+		
+//		StringBuilder nomeSB = new StringBuilder("Alura");
+//		nomeSB.reverse();
+		
+		System.out.println(nome);
+//		System.out.println(outro);
+//		System.out.println(nomeSB);
+	}
+}
